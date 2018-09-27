e7d5db6f14b7a0b0caa37788eee9cb17d3ba5c7d
pc\goodpc <duvnjak.filip5@gmail.com>
2018-09-27 10:55:51 +0200

    Dopoljevanje classov + enum

--- Monopili/Monopili/Koda/Polje.cpp
+++ Monopili/Monopili/Koda/Polje.cpp
@@ -4,3 +4,9 @@ SDL_Rect IPolje::getKvadrat()
 {
 	return KvadratPolje;
 }
+
+PoljeZAkcijo::PoljeZAkcijo(int i)
+{
+	//if (i == 2){
+	//}
+}
--- Monopili/Monopili/Koda/Polje.h
+++ Monopili/Monopili/Koda/Polje.h
@@ -3,46 +3,108 @@
 
 #include "SDL_Stuff.h"
 #include "Igralec.h"
+#include "Igra.h"
 
+enum Akciija
+{
+	Zacetek,
+	DrzavnaBanka,
+	DavekNaDohodek,
+	Piloznost,
+	Zapor,
+	Parking,
+	PojdiZapor,
+	DavekNaPremozenje
+};
+
+enum Posebno
+{
+	Zahod,
+	Elektrika,
+	Sever,
+	Vzhod,
+	Voda,
+	Jug
+};
 
 class IPolje {
-private:
+protected:
 	int ID;
 	SDL_Rect KvadratPolje;//polje mesta
 public:
 	SDL_Rect getKvadrat();//vrne rect vrednosti
-	void SetKvadraat(SDL_Rect );
 	//virtual void action() = 0;
 	virtual void narisi() = 0;
 };
 
+/*referenca oz. zaèasno
+
+class IgralnaPovrsina : IPolje {
+public:
+	IgralnaPovrsina() {
+		int w, h;
+		ID = 0;
+		SDL_GetWindowSize(SDL_Window* ImeNasegaOkna, w, h);//zdi se mi da tko nekako dela e ne pa mal priredim
+		KvadratPolje.x = (w / 16) * (9/2);
+		KvadratPolje.y = (h / 9);
+		KvadratPolje.width = (w / 16) * 7; //razmerna širina igrlane plošèe
+		if (((h/9)*5) < KvadratPolje.width)
+		{
+			KvadratPolje.height = (h / 9) * 5;
+			KvadratPolje.width = (w / 16) * 7;
+		}
+		else
+		{
+			KvadratPolje.height = KvadratPolje.width;
+
+		}
+	}
+};
+
+Konec reference*/
+
 class PoljeZAkcijo: IPolje {
 private:
 	SDL_Texture* Slikca;
+	int Vrsta_Akcije;//enum Akcija
 public:
+	PoljeZAkcijo(int i);//konstruktor
 	void narisi();
 };
+
+/*dodano
+class PoljeKot :PoljeZAkcijo {
+	int Koti;//doloèi kateri kot (0=start,1=Zapor,2=Parking,3=Pojdi v Zapor)
+
+};
+*/
 
 class ILastniskoPolje: IPolje {
 private:
 	int cena;
+	int IndexIgralec;
 public:
 	virtual void narisi() = 0;
 };
 
 class GradbenoPolje: ILastniskoPolje {
-private:
+protected:
 	//barva
-	SDL_Rect KvadratBarva;//Katero barveno skupino spada
+	SDL_Rect KvadratBarva;//Katero barveno skupino spada izrisat
 	int cenaHiske;
+	int cenaHotela;
+	int BarvenoSkupina;
 public:
+	GradbenoPolje(int i);//konstruktor
 	void narisi();
 };
 
 class PosebnoPolje: ILastniskoPolje {
 private:
 	SDL_Texture * Slikca;
+	int VrstaPolja;//enum Posebno
 public:
+	PosebnoPolje(int i);//konstruktor
 	void narisi();
 };
 
